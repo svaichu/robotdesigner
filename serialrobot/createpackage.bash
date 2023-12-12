cd54851b3e4e9797bb84a0ef3243a7f43186a956
svaichu <s.vaichu@gmail.com>
2023-12-12 11:59:38 +0100

    change bash scripts and urdf write func

--- serialrobot/createpackage.bash
+++ serialrobot/createpackage.bash
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 
 package_name="first"
+ws_name="my_ws
 
 source /opt/ros/humble/setup.bash
 cd ~
-mkdir -p ros2_ws/src
-cd ros2_ws/src
+mkdir -p $ws_name/src
+cd $ws_name/src
 [ ! -d "$package_name" ] && ros2 pkg create $package_name --build-type ament_python --dependencies rclpy
 cd $package_name
 echo "entered into directory"
 mkdir -p urdf
 mkdir -p rviz
-cd ~/ros2_ws
+cd ~/$ws_name
