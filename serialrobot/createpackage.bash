6694226efd898f1106c48dd633540c8d3671ea03
svaichu <s.vaichu@gmail.com>
2023-12-13 19:03:40 +0100

    Fix typo in ws_name variable assignment

--- serialrobot/createpackage.bash
+++ serialrobot/createpackage.bash
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 package_name="first"
-ws_name="my_ws
+ws_name="my_ws"
 
 source /opt/ros/humble/setup.bash
 cd ~
@@ -9,7 +9,6 @@ mkdir -p $ws_name/src
 cd $ws_name/src
 [ ! -d "$package_name" ] && ros2 pkg create $package_name --build-type ament_python --dependencies rclpy
 cd $package_name
-echo "entered into directory"
 mkdir -p urdf
 mkdir -p rviz
 cd ~/$ws_name
