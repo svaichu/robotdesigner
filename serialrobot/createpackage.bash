0b521341177879ccd890990fa6b89fc01e38c541
svaichu <s.vaichu@gmail.com>
2023-12-13 22:14:08 +0100

    Update package name and add default value for input

--- serialrobot/createpackage.bash
+++ serialrobot/createpackage.bash
@@ -3,6 +3,13 @@
 package_name="first"
 ws_name="my_ws"
 
+if [[ -n "$1" ]]; then
+    package_name=$1
+else
+    package_name="robot"
+    echo "no input, using default ws $ws_name"
+fi
+
 source /opt/ros/humble/setup.bash
 cd ~
 mkdir -p $ws_name/src
