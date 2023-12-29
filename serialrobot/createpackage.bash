6cde37269e2f36a60450b471febd71770aab8913
svaichu <s.vaichu@gmail.com>
2023-12-29 19:47:00 +0100

    Update default package name in addrviz.bash and createpackage.bash scripts

--- serialrobot/createpackage.bash
+++ serialrobot/createpackage.bash
@@ -7,7 +7,7 @@ if [[ -n "$1" ]]; then
     package_name=$1
 else
     package_name="robot"
-    echo "no input, using default ws $ws_name"
+    echo "no input, using default package name $package_name"
 fi
 
 source /opt/ros/humble/setup.bash
